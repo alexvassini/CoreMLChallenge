800b1f85160d0b66d4bea1a686bbc5f92d760ae1
Alexandre Vassinievski <alexvassini@gmail.com>
2018-02-25 11:08:57 -0300

    camera view + assets + coreML model

--- CoreMLChallenge/ViewController.swift
+++ CoreMLChallenge/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var mapView: MKMapView!
+  
+  var locationHelper = LocationHelper()
+  let regionRadius: CLLocationDistance = 800
+  var lastLocation: CLLocation? {
+    didSet{
+      centerMapOnLocation(location: lastLocation!)
+      //myPosition.coordinate = (lastLocation?.coordinate)!
+    }
+  }
+ // let myPosition = MKPointAnnotation()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    locationHelper.listener = self
+    locationHelper.requestLocation(onCompletion:nil)
+    //mapView.addAnnotation(myPosition)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func centerMapOnLocation(location: CLLocation) {
+   // let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius, regionRadius)
+    
+    let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+    
+    mapView.setRegion(region, animated: true)
   }
-
 
 }
 
+
+extension ViewController: LocationHelperListener {
+  func didUpdateLocation(lastLocation: CLLocation) {
+    self.lastLocation = lastLocation
+  }
+  
+}
